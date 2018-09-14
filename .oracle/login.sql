814f4ca2bf3c5951377b7a9b6af776c6d51cfbaa
Rubin Simons <rubin@raaf.tech>
2018-09-14 14:36:01 +0200

    Added Oracle login.sql.

---
+++ .oracle/login.sql
@@ -0,0 +1,7 @@
+set lin 32767 trimsp on tab off
+set emb on pages 0 newp none
+def _editor=code
+set editf /tmp/lscfile.sql
+set long 1000000000 longc 60000
+set serverout on size unlimited
+set sqlp "_CONNECT_IDENTIFIER> "
