572cb5e989ed9846e093cbfbdbea11f0bd27678f
Rubin Simons <rubin@raaf.tech>
2021-03-01 09:59:46 +0100

    Spurious i.

--- .profile.d/user-man.sh
+++ .profile.d/user-man.sh
@@ -6,7 +6,7 @@ platform=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 if [[ "$platform" == "linux" ]]; then
     # Default manpath.
-i   MANPATH="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/snap/man"
+    MANPATH="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/snap/man"
 
     # Extra manpath.
     EXTRA_MANPATH="/opt/audacious/audacious3/share/man:/opt/docker/docker1903/share/man:/opt/imagemagick/im7/share/man:/opt/java/default/man:/opt/leanux/leanux1/man:/opt/rabbitmq/rmq38/share/man:/opt/scala/scala2/man"
