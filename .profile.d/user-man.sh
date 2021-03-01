7d3a86ec602e361af9965e7730b2761239c366d5
Rubin Simons <rubin@raaf.tech>
2021-03-01 09:56:54 +0100

    Separate manpath setting, like pkgbuild.

---
+++ .profile.d/user-man.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# What are we running on?
+platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+[[ "$(uname -r)" =~ "icrosoft" ]] && platform=windows
+
+if [[ "$platform" == "linux" ]]; then
+    # Default manpath.
+i   MANPATH="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/snap/man"
+
+    # Extra manpath.
+    EXTRA_MANPATH="/opt/audacious/audacious3/share/man:/opt/docker/docker1903/share/man:/opt/imagemagick/im7/share/man:/opt/java/default/man:/opt/leanux/leanux1/man:/opt/rabbitmq/rmq38/share/man:/opt/scala/scala2/man"
+
+    export MANPATH="$MANPATH:$EXTRA_MANPATH"
+fi
