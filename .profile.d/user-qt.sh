2229714084d0ca7bfa787a521dc121e788295328
Rubin Simons <rubin@raaf.tech>
2021-12-18 02:35:22 +0100

    Set wayland or xcb based on XDG_SESSION_TYPE.

--- .profile.d/user-qt.sh
+++ .profile.d/user-qt.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$(uname -s)" == "Linux" ]; then
-    export QT_QPA_PLATFORM=wayland
+    if [ "$XDG_SESSION_TYPE" == "x11" ]; then
+        export QT_QPA_PLATFORM=xcb
+    elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+        export QT_QPA_PLATFORM=wayland
+    fi
     export QT_AUTO_SCREEN_SCALE_FACTOR=1
     export QT_QPA_PLATFORMTHEME=qt5ct
     #export QT_STYLE_OVERRIDE=''
