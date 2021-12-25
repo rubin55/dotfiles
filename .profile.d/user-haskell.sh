0c5ba1376e7cdbe235419ad149203b3bac92e417
Rubin Simons <rubin@raaf.tech>
2021-12-25 15:43:33 +0100

    Started using ghcup for haskell instead of stack-only.

--- .profile.d/user-haskell.sh
+++ .profile.d/user-haskell.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-[[ $(which stack 2>/dev/null) ]] && STACK_EXEC=$(which stack 2>/dev/null) && alias stack="$STACK_EXEC"
-[[ $(which stack.bat 2>/dev/null) ]] && STACK_EXEC=$(which stack.bat 2>/dev/null) && alias stack="cmd.exe /c stack.bat"
+# If we have ghcpup, set up haskell using ghcup.
+[ -f "/home/rubin/.ghcup/env" ] && source "/home/rubin/.ghcup/env"
 
-if [[ ! "$STACK_EXEC" =~ "bat" ]]; then
-    export STACK_ROOT="$HOME/.stack/root"
-fi
