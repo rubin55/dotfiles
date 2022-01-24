a4bf1138bc4fae63c30f5a6b315e813f02db0016
Rubin Simons <me@rubin55.org>
2022-01-24 23:05:02 +0100

    Fix manpath extension for node.

--- .profile.d/user-node.sh
+++ .profile.d/user-node.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+function addManPaths() {
+    NVM_DEFAULT_ALIAS="$HOME/.nvm/alias/default"
+    if [ -e "$NVM_DEFAULT_ALIAS" ]; then
+        NODE_VERSION=$(cat "$NVM_DEFAULT_ALIAS")
+    else
+        NODE_VERSION=$(node --version)
+    fi
+
+    MANPATH_CANDIDATES="$HOME/.nvm/versions/node/$NODE_VERSION/share/man:$HOME/.nvm/versions/node/$NODE_VERSION/lib/node_modules/npm/man"
+
+    # Check if candidates exist, only add those.
+    OLD_IFS=$IFS ; IFS=:
+    for MANPATH_ELEMENT in $MANPATH_CANDIDATES; do
+        if [[ -d "$MANPATH_ELEMENT" && ":$MANPATH_NEW:" != *":$MANPATH_ELEMENT:"* ]]; then
+            MANPATH="${MANPATH:+"$MANPATH:"}$MANPATH_ELEMENT"
+        fi
+    done
+    IFS=$OLD_IFS
+
+    # Make sure we don't have empty entries.
+    export MANPATH=$(echo "$MANPATH" | sed "s|::|:|g")
+
+}
+
 # If we have nvm, set up node using nvm, else do some manual magic.
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
     source "$HOME/.nvm/nvm.sh"
-    # Add npm man pages to MANPATH if not there already
-    NODE_VERSION=$(node --version | sed 's/^v//')
-    NPM_MANPATH="$HOME/.nvm/versions/node/v$NODE_VERSION/lib/node_modules/npm/man"
-    if [[ ":$MANPATH:" != *":$NPM_MANPATH:"* ]]; then
-        export MANPATH="${MANPATH:+"$MANPATH:"}$NPM_MANPATH"
-    fi
+    addManPaths
 else
     # A few helpful variables.
     [[ $(which node 2>/dev/null) ]] && NODE_EXEC=$(which node 2>/dev/null) && alias node="$(echo $NODE_EXEC | sed -e 's| |\\ |g' -e 's|(|\\(|g' -e 's|)|\\)|g')"
@@ -34,11 +53,8 @@ else
             export PATH="${PATH:+"$PATH:"}$NPM_CURRENT_PREFIX/bin"
         fi
 
-        # Add npm man pages to MANPATH if not there already
-        NPM_MANPATH="$HOME/.nvm/versions/node/v$NODE_VERSION/lib/node_modules/npm/man"
-        if [[ ":$MANPATH:" != *":$NPM_MANPATH:"* ]]; then
-            export MANPATH="${MANPATH:+"$MANPATH:"}$NPM_MANPATH"
-        fi
+        # Add man pages to manpath.
+        addManPaths
     fi
 fi
 
