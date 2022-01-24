dc77fe49bc472628b664d070159dbf8a0138f956
Rubin Simons <me@rubin55.org>
2022-01-24 23:04:10 +0100

    Handle man path extension for ghcup.

--- .profile.d/user-haskell.sh
+++ .profile.d/user-haskell.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 # If we have ghcpup, set up haskell using ghcup.
-[ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env"
+if [ -f "$HOME/.ghcup/env" ]; then
+    source "$HOME/.ghcup/env"
 
+    # Add manpath for ghc too.
+    GHC_MANPATH="$( echo "$(ghcup -o whereis --directory ghc)" | sed 's|/bin|/share/man|g')"
+    if [[ -d "$GHC_MANPATH" && ":$MANPATH:" != *":$GHC_MANPATH:"* ]]; then
+        MANPATH="${MANPATH:+"$MANPATH:"}$GHC_MANPATH"
+    fi
+
+    # Make sure we don't have empty entries.
+    export MANPATH=$(echo "$MANPATH" | sed "s|::|:|g")
+fi
