b6791f6e5fb532a6222d89a25151aa337e00901c
Rubin Simons <me@rubin55.org>
2022-01-24 23:04:44 +0100

    Clean up generic manpath extension.

--- .profile.d/user-man.sh
+++ .profile.d/user-man.sh
@@ -6,20 +6,15 @@ MANPATH_CANDIDATES="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:
 # Check if candidates exist, only add those.
 OLD_IFS=$IFS ; IFS=:
 for MANPATH_ELEMENT in $MANPATH_CANDIDATES; do
-    if [ -d "$MANPATH_ELEMENT" ]; then
-        MANPATH_NEW+="$MANPATH_ELEMENT:"
+    if [[ -d "$MANPATH_ELEMENT" && ":$MANPATH_NEW:" != *":$MANPATH_ELEMENT:"* ]]; then
+        MANPATH_NEW="${MANPATH_NEW:+"$MANPATH_NEW:"}$MANPATH_ELEMENT"
     fi
 done
 IFS=$OLD_IFS
 
-# Extra manpath entry for node/nvm, needs a version lookup..
-NVM_DEFAULT_ALIAS="$HOME/.nvm/alias/default"
-if [ -e "$NVM_DEFAULT_ALIAS" ]; then
-    NVM_DEFAULT_VERSION=$(cat $NVM_DEFAULT_ALIAS)
-    MANPATH_NEW+="$HOME/.nvm/versions/node/$NVM_DEFAULT_VERSION/share/man:"
-fi
-
 # Extra manpath elements for programs in /opt.
-MANPATH_NEW+="$(find /opt -maxdepth 5 -type d -name man 2>/dev/null| tr '\n' ':')"
+MANPATH_NEW+=":$(find /opt -maxdepth 5 -type d -name man 2>/dev/null| tr '\n' ':')"
 
-export MANPATH=$MANPATH_NEW
+# Make sure we don't have empty entries.
+export MANPATH=$(echo "$MANPATH_NEW" | sed "s|::|:|g")
+
