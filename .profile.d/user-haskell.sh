976ca49e81f164cce7c5c6e818bc5712a8ff7b39
Rubin Simons <me@rubin55.org>
2022-05-17 00:45:33 +0200

    Make sure we include the right ghc version for inclusion on MANPATH.

--- .profile.d/user-haskell.sh
+++ .profile.d/user-haskell.sh
@@ -5,11 +5,16 @@ if [ -f "$HOME/.ghcup/env" ]; then
     source "$HOME/.ghcup/env"
 
     # Add manpath for ghc too.
-    GHC_MANPATH="$(echo "$(ghcup -o whereis --directory ghc)" | sed 's|/bin|/share/man|g')"
-    if [[ -d "$GHC_MANPATH" && ":$MANPATH:" != *":$GHC_MANPATH:"* ]]; then
-        MANPATH="${MANPATH:+"$MANPATH:"}$GHC_MANPATH"
-    fi
+    GHC_BINARY="$(which ghc 2> /dev/null)"
+    if [ -x "$GHC_BINARY" ]; then
+        GHC_VERSION="$(ghc --version | awk 'NF>1{print $NF}')"
+        GHC_MANPATH="$(echo "$(ghcup -o whereis --directory ghc $GHC_VERSION)" | sed 's|/bin|/share/man|g')"
+        if [[ -d "$GHC_MANPATH" && ":$MANPATH:" != *":$GHC_MANPATH:"* ]]; then
+            MANPATH="${MANPATH:+"$MANPATH:"}$GHC_MANPATH"
+        fi
 
-    # Make sure we don't have empty entries.
-    export MANPATH=$(echo "$MANPATH" | sed "s|::|:|g")
+        # Make sure we don't have empty entries.
+        export MANPATH=$(echo "$MANPATH" | sed "s|::|:|g")
+
+    fi
 fi
