4afb5264a780fe96bd013224730f3faee753e4d2
Rubin Simons <me@rubin55.org>
2023-08-03 07:49:30 +0300

    Well, pgrep is not required.

--- .profile.d/user-libvirt.sh
+++ .profile.d/user-libvirt.sh
@@ -2,7 +2,7 @@
 
 # Check if functions are loaded and if required executables are available.
 type -p os.platform path.which || return
-path.which pgrep,libvirtd || return
+path.which libvirtd || return
 
 # Set LIBVIRT_DEFAULT_URI when libvirtd is running.
 export LIBVIRT_DEFAULT_URI=qemu:///system
