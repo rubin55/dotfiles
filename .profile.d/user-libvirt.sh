a09d2e9ddb8cfa64f5df6e2e4eb126f0d26e1e92
Rubin Simons <me@rubin55.org>
2023-08-03 07:31:51 +0300

    Just having libvirtd on path is enough.

--- .profile.d/user-libvirt.sh
+++ .profile.d/user-libvirt.sh
@@ -5,6 +5,4 @@ type -p os.platform path.which || return
 path.which pgrep,libvirtd || return
 
 # Set LIBVIRT_DEFAULT_URI when libvirtd is running.
-if [[ "$(os.platform)" == "linux" && "$(pgrep libvirtd)" =~ ^[0-9]+$ ]]; then
-  export LIBVIRT_DEFAULT_URI=qemu:///system
-fi
+export LIBVIRT_DEFAULT_URI=qemu:///system
