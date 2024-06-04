80904cf6ee08870c08677eca45d1db202705351e
Rubin Simons <me@rubin55.org>
2024-06-05 00:13:11 +0200

    Correctly set centaur options after load.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -276,7 +276,7 @@
   (setq evil-escape-key-sequence nil))
 
 ;; Show emacs version after startup.
-(add-hook 'window-setup-hook (lambda () (run-with-timer 1.1 nil #'call-interactively 'version)))
+(add-hook 'window-setup-hook (lambda () (run-with-timer 1.2 nil #'call-interactively 'version)))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
