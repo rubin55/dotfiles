33fcba94b7e16bcd126e82a3d3226315a019752a
Rubin Simons <me@rubin55.org>
2024-09-11 21:16:56 +0200

    Added line and column number to statuslinne.

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "rosepine-dawn",
+	theme = "rosepine",
   theme_toggle = { "rosepine", "rosepine-dawn" },
 
 	hl_override = {
@@ -17,12 +17,38 @@ M.base46 = {
 
 M.ui = {
   statusline = {
-      -- default/vscode/vscode_colored/minimal
-      theme = "default",
-      -- default/round/block/arrow 
-      separator_style = "block",
-    },
-    nvdash = {
+    -- default/vscode/vscode_colored/minimal
+    theme = "default",
+    -- default/round/block/arrow 
+    separator_style = "block",
+
+    -- statusline with line and col position
+    modules = {
+      cursor = function()
+        local fn = vim.fn
+        local sep_l = "█"
+        local line = fn.line "."
+        local col = fn.virtcol "."
+
+        local chad = string.format(" %d:%d", line, col) .. " │ "
+
+        -- default cursor_position module
+        local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
+
+        local current_line = fn.line "."
+        local total_line = fn.line "$"
+        local text = math.modf((current_line / total_line) * 100) .. tostring "%%"
+        text = string.format("%4s", text)
+
+        text = (current_line == 1 and "Top") or text
+        text = (current_line == total_line and "Bot") or text
+
+        return left_sep .. "%#St_pos_text#" .. chad .. text .. " "
+      end,
+    }
+  },
+
+  nvdash = {
       load_on_startup = true,
   },
 }
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+
+-- enable cursorline
+o.cursorlineopt ='both'
