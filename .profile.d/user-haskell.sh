711d307f2b7ff741dc5f02931938b09131b76cec
Rubin Simons <me@rubin55.org>
2024-09-17 23:46:47 +0200

    Make user-haskell.sh parsing work.

--- .profile.d/user-haskell.sh
+++ .profile.d/user-haskell.sh
@@ -2,27 +2,25 @@
 
 # Check if functions are loaded and if required executables are available.
 type -p path.append path.which || return
-path.which awk ln || return
+path.which awk,ln || return
 
-# If we have ghcpup, set up haskell using ghcup.
-if [[ -f "$HOME/.ghcup/env" ]]; then
-  source "$HOME/.ghcup/env"
-
-  # Figure out manpath location.
-  if path.which ghc; then
-    GHC_VERSION="$(ghc --version | awk 'NF>1{print $NF}')"
-    MANPATH_NEW="$HOME/.ghcup/ghc/$GHC_VERSION/share/doc/ghc-$GHC_VERSION/users_guide/build-man"
-    if [[ -d "$MANPATH_NEW" ]]; then
-      if [[ ! -L "$MANPATH_NEW/man1" ]]; then
-        cd "$MANPATH_NEW"
-        ln -s . man1
-        cd - > /dev/null 2>&1
-      fi
+# If we find ghc in .ghcup/bin, we add it to the path.
+# Also try to add manpath. Note, not all ghc from ghcup
+# have man pages.
+if path.which ghc "$HOME/.ghcup/bin"; then
+  export PATH="$(path.append "$HOME/.ghcup/bin" "$PATH")"
+  GHC_VERSION="$(ghc --version | awk 'NF>1{print $NF}')"
+  MANPATH_NEW="$HOME/.ghcup/ghc/$GHC_VERSION/share/doc/ghc-$GHC_VERSION/users_guide/build-man"
+  if [[ -d "$MANPATH_NEW" ]]; then
+    if [[ ! -L "$MANPATH_NEW/man1" ]]; then
+      cd "$MANPATH_NEW"
+      ln -s . man1
+      cd - >/dev/null 2>&1
     fi
-
-    # Add manpath (prefixed ':' is intentional, see man manpath).
-    export MANPATH=":$(path.append "$MANPATH_NEW" "$MANPATH")"
   fi
+
+  # Add manpath (prefixed ':' is intentional, see man manpath).
+  export MANPATH=":$(path.append "$MANPATH_NEW" "$MANPATH")"
 fi
 
 # Unset temporary variables.
