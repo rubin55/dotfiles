987d3e9b58519f04c674d548aeb65f203bbf5152
Rubin Simons <me@rubin55.org>
2024-09-18 15:59:42 +0200

    Added default options for neovide.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,8 +1,14 @@
 require "nvchad.options"
 
 -- add yours here!
-
+local g = vim.g
 local o = vim.o
 
 -- enable cursorline
 o.cursorlineopt ='both'
+
+-- neovide settings
+if vim.g.neovide then
+  g.neovide_hide_mouse_when_typing = true
+  g.neovide_cursor_animation_length = 0
+end
