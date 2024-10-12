dcf50a4d7586d322ebfc2862078cfaf0323ebb16
Rubin Simons <me@rubin55.org>
2024-10-12 09:58:48 +0200

    A few nvim tweaks.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,9 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
+-- set title
+-- vim.opt.title = true
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,6 +4,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "<leader>cc", ":lua cycle_colorcolumn()<cr>", { desc = "Cycle Line length indicator" })
-map("n", "<leader>tt", ":lua require('base46').toggle_theme()<cr>", { desc = "Toggle Theme" })
-map("n", "<f11>", ":lua toggle_fullscreen()<cr>", { desc = "Toggle Full Screen" })
+map("n", "<leader>cc", ":lua cycle_colorcolumn()<cr>", { desc = "cycle line length indicator" })
+map("n", "<leader>tt", ":lua require('base46').toggle_theme()<cr>", { desc = "toggle dark/light theme" })
+map("n", "<f11>", ":lua toggle_fullscreen()<cr>", { desc = "toggle full screen" })
