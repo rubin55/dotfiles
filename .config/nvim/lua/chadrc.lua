aedf28cbd47bf79f4b745caa59bcac1e1a06f721
Rubin Simons <me@rubin55.org>
2025-05-05 14:37:06 +0200

    Github Light for nvim.

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "poimandres",
-  theme_toggle = { "poimandres", "rosepine-dawn" },
+	theme = "github_light",
+  theme_toggle = { "poimandres", "github_light" },
 
 	hl_override = {
 	  Comment = { italic = true },
