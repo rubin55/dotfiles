a4eb83390c17e3a065dd3c2da49d0d72ae6e05e3
Rubin Simons <me@rubin55.org>
2025-06-02 15:00:57 +0200

    Set EDITOR to zed

--- .profile.d/user-various.sh
+++ .profile.d/user-various.sh
@@ -23,7 +23,7 @@ if path.which dircolors; then
 fi
 
 # Set default editor.
-path.which code && export EDITOR='code -w'
+path.which zeditor && export EDITOR='zeditor -w'
 
 # Set up less to use lessfilter (pygments).
 export PAGER='less'
