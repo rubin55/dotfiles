49d00d711547550f73c4129c36f3dbad14974a58
Rubin Simons <me@rubin55.org>
2025-10-23 12:05:04 +0200

    Removed erronuous env var for dotnet, added no telemetry env var for crush

--- .profile.d/user-crush.sh
+++ .profile.d/user-crush.sh
@@ -2,8 +2,8 @@
 
 # Check if functions are loaded and if required executables are available.
 type -p path.which || return
-path.which dotnet || return
+path.which crush || return
 
-# Opt out of dotnet telemetry.
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+# Opt out of crush telemetry.
+export CRUSH_DISABLE_METRICS=1
 
