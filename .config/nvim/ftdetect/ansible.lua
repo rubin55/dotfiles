b7460e84ad3beb5d4eb0a776f87ba071536bc8dd
Rubin Simons <me@rubin55.org>
2026-04-06 14:50:45 +0300

    Better ansible ftdetect rules

--- .config/nvim/ftdetect/ansible.lua
+++ .config/nvim/ftdetect/ansible.lua
@@ -1,14 +1,19 @@
 vim.filetype.add({
   pattern = {
-    [".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+    -- Top-level project directories.
     [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
     [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
-    [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
-    [".*/playbook.*%.ya?ml"] = "yaml.ansible",
     [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
-    [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
-    [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    -- Playbook files by naming convention.
+    [".*/playbook.*%.ya?ml"] = "yaml.ansible",
+    -- Role directories (valid at any nesting level).
     [".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+    [".*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    [".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+    -- Role-only directories (require roles/ ancestor).
+    [".*/roles/.*/vars/.*%.ya?ml"] = "yaml.ansible",
+    [".*/roles/.*/meta/.*%.ya?ml"] = "yaml.ansible",
+    -- Molecule testing
     [".*/molecule/.*%.ya?ml"] = "yaml.ansible",
   },
 })
