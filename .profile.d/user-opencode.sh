573296df51c93c71c3a61ecdc02a3d184f2b1710
Rubin Simons <me@rubin55.org>
2026-04-20 10:05:45 +0200

    Added disable title set for opencode

--- .profile.d/user-opencode.sh
+++ .profile.d/user-opencode.sh
@@ -1,5 +1,13 @@
+# Check if functions are loaded and if required executables are available.
+type -p path.which || return
+
+# Check if opencode is available.
+path.which opencode || return
+
+
 export OPENCODE_DISABLE_CLAUDE_CODE=1
 export OPENCODE_DISABLE_CLAUDE_CODE_PROMPT=1
 export OPENCODE_DISABLE_CLAUDE_CODE_SKILLS=1
 export OPENCODE_DISABLE_EXTERNAL_SKILLS=1
 export OPENCODE_DISABLE_LSP_DOWNLOAD=true
+export OPENCODE_DISABLE_TERMINAL_TITLE=true
